68f92add9dbbe8feb97d322a639e668407490f50
uehoho18 <ramune276@gmail.com>
2022-08-28 23:50:32 +0900

    カメラ部分の実装

--- lib/view/post/screens/post_upload_screen.dart
+++ lib/view/post/screens/post_upload_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:slr_sharemap/utils/constants.dart';
+import 'package:slr_sharemap/view_models/post_view_model.dart';
 
 class PostUploadScreen extends StatelessWidget {
   final UploadType uploadType;
@@ -7,6 +9,14 @@ class PostUploadScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //TODO [provider4.1.0] Provider.of(listen: false) => context.read()
+    Future(() {
+      final postViewModel = context.read<PostViewModel>();
+      if (!postViewModel.isImagePicked && !postViewModel.isProcessing) {
+        postViewModel.pickImage(uploadType);
+      }
+    });
+
     return Scaffold(
       body: Container(),
     );
