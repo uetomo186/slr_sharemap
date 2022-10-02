5021d59230347ed0d4ae8b75af0d1241ba13bd25
uehoho18 <ramune276@gmail.com>
2022-10-02 23:46:16 +0900

    [app]locationのデータを追加

--- lib/models/location/location.manager.dart
+++ lib/models/location/location.manager.dart
@@ -1,12 +1,19 @@
+import 'dart:async';
+
+import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:slr_sharemap/data_models/location.dart';
 
 class LocationManager {
   Future<Location> getCurrentLocation() async {
+    //↓↓↓↓↓↓↓ geolocatorバージョン8.0以上で追記が必要な部分開始 ↓↓↓↓↓↓↓
+    //（パーミッションチェック（Geolocator.checkPermission()）の追加要）
+    //https://pub.dev/packages/geolocator/changelog#800
+    //https://pub.dev/packages/geolocator#example
+
     final isLocationServiceEnabled =
         await Geolocator.isLocationServiceEnabled();
     if (!isLocationServiceEnabled) {
-      return Future.error("位置情報サービスがオフになっています。");
+      return Future.error("位置情報サービスがオフになっています");
     }
     var permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
@@ -16,7 +23,42 @@ class LocationManager {
       }
     }
     if (permission == LocationPermission.deniedForever) {
-      return Future.error("位置情報へのアクセス許可がずっと拒否され続けております。");
+      return Future.error("位置情報へのアクセスが永久に拒否されており、リクエストすらできません。");
     }
+
+    final position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
+
+    final placeMarks = await geoCoding.placemarkFromCoordinates(
+      position.latitude,
+      position.longitude,
+    );
+    final placeMark = placeMarks.first;
+    return Future.value(
+      convert(
+        placeMark,
+        position.latitude,
+        position.longitude,
+      ),
+    );
+  }
+
+  Future<Location> updateLocation(double latitude, double longitude) async {
+    final placeMarks =
+        await geoCoding.placemarkFromCoordinates(latitude, longitude);
+    final placeMark = placeMarks.first;
+    return Future.value(convert(placeMark, latitude, longitude));
+  }
+
+  Location convert(
+      geoCoding.Placemark placeMark, double latitude, double longitude) {
+    return Location(
+      latitude: latitude,
+      longitude: longitude,
+      country: placeMark.country ?? "",
+      state: placeMark.administrativeArea ?? "",
+      city: placeMark.locality ?? "",
+    );
   }
 }
